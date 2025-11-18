7078bc304b6bcef0d8d17026c0a856c77cf470a5
LEI-122676 <tfcss2@iscte-iul.pt>
2025-11-18 13:49:52 +0000

    +Enviroment

--- Observation.py
+++ Observation.py
@@ -4,3 +4,5 @@ class Observation:
         self.agent_id = agent_id    # ID of the agent for whom this observation is made
         self.data = []              # Placeholder for observation data (matrix of whatever is observable from the Agent's POV)
 
+    def addData(self, data):
+        self.data.append(data)
--- Terrain.py
+++ Terrain.py
@@ -1,29 +1,40 @@
 import random
-from threading import Lock
 
-from Egg import Egg
-from ExplorerAgent import ExplorerAgent
 from Chicken import Chicken
+from Egg import Egg
+from Environment import Environment
+from ExplorerAgent import ExplorerAgent
 from Observation import Observation
 
 
-class Map:
+class Terrain(Environment):
 
     def __init__(self, width=100, height=100):
         self.width = width
         self.height = height
+        self.solved = False
 
         self.map = [["" for _ in range(width)] for _ in range(height)]
-
-        self.chickens = []                                                      # Phase 3
+        self.chickens = []  # Phase 3
         self.eggs = []
         self.nests = []
         self.stones = []
 
-        self.lock = Lock()
-        self.solved = False
 
-    def addToMap(self, numEggs, numNests, numChickens):
+    def observationFor(self, explorer: ExplorerAgent):  # Phase 5.2
+        obs = Observation()
+
+        # TODO - usar Sensor?
+
+        return explorer.observation(obs)
+
+    def update(self):
+        pass
+
+    def act(self, action, agent):
+        pass
+
+    def initializeMap(self, numEggs, numNests, numChickens):
         # TODO - corrigir isto, pq podem acontecer sobreposicoes de elementos com os randoms nao verificados
 
         for n in range(numEggs):
@@ -43,25 +54,6 @@ class Map:
         for n in range(numChickens):
             self.chickens.append(Chicken(n, 0, n))
 
-    def observationFor(self, explorer: ExplorerAgent):                          # Phase 5.2
-        obs = Observation()
-        
-        # TODO - usar Sensor?
-        
-        return explorer.observation(obs)
-
-    def update(self):
-        pass
-
-    def act(self, action, agent):
-        with self.lock:
-            # Calculates future position
-            future = (agent.x + action[0], agent.y + action[1])
-
-            # Check if future position is within map bounds
-            if self.isActionValid(future):
-                agent.act(action)
-
     def isActionValid(self, future):
         x, y = future
         if x < 0 or x >= len(self.map[0]) or y < 0 or y >= len(self.map):
@@ -71,4 +63,3 @@ class Map:
             return False
 
         return True
-
