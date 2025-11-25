3e8fc344516d9fbeb3aeec91c5bf9cd5cecacf66
LEI-122676 <tfcss2@iscte-iul.pt>
2025-11-25 14:24:51 +0000

    +8 directions

--- Observation.py
+++ Observation.py
@@ -3,10 +3,3 @@ class Observation:
     def __init__(self, agent_id):
         self.agent_id = agent_id
         self.possible_actions = []
-
-    def addData(self, data):
-        self.data.append(data)
-
-    def getData(self):
-        for d in self.data:
-            yield d
