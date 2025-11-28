3398fdedadc2f77f3345a60f0fe1db60ceda1851
LEI-122676 <tfcss2@iscte-iul.pt>
2025-11-28 09:39:04 +0000

    more World.act() logic

--- Items/Nest.py
+++ Items/Nest.py
@@ -6,16 +6,16 @@ class Nest(Item):
     def __init__(self, id, x, y, capacity: int):
         super().__init__("N", id, x, y)
         self.capacity = capacity
-        self.eggs = 0
+        self.num_of_items = 0
 
     # Returns True when egg is put in the Nest
-    def putEgg(self, numEggs):
-        future = self.eggs + numEggs
+    def put(self, num_of_items):
+        future_num_of_items = self.num_of_items + num_of_items
 
-        if future < self.capacity:
-            self.eggs = future
-            print(f"[{self}] - Has {self.eggs} eggs.")
+        if future_num_of_items < self.capacity:
+            self.num_of_items = future_num_of_items
+            print(f"[{self}] - Has {self.num_of_items} eggs.")
             return True
         else:
-            print(f"[{self}] - Cannot put {numEggs} eggs. Capacity is currently at {self.eggs}/{self.capacity}.")
+            print(f"[{self}] - Cannot put {num_of_items} eggs. Capacity is currently at {self.num_of_items}/{self.capacity}.")
             return False
