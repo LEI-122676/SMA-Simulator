989297e8f4c2417953dbb1231e72e681caa88341
LEI-122676 <tfcss2@iscte-iul.pt>
2025-11-29 17:11:39 +0000

    -problems

--- Items/Wall.py
+++ Items/Wall.py
@@ -2,5 +2,5 @@ from Items.Item import Item
 
 class Wall(Item):
 
-    def __init__(self, x: int, y: int):
-        super().__init__("W", x, y)
+    def __init__(self, id, x, y):
+        super().__init__("W", id, x, y)
--- demo.py
+++ demo.py
@@ -2,9 +2,7 @@ from Simulator.SimulatorMotor import SimulatorMotor
 
 
 def main():
-    simulator_farol = SimulatorMotor()
-    file_path = "example_file_farol.txt"
-    simulator_farol.create(file_path)
+    simulator_farol = SimulatorMotor.create("example_file_farol.txt")
     simulator_farol.execute()
 
 if __name__ == "__main__":
