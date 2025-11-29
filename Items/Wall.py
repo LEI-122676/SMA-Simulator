d4053398432c0970c0ef0829f8131f1d6beca3f2
Catarina Figueiredo <csfoe@iscte-iul.pt>
2025-11-29 16:21:50 +0000

    diagrama

--- Items/Wall.py
+++ Items/Wall.py
@@ -3,7 +3,4 @@ from Items.Item import Item
 class Wall(Item):
 
     def __init__(self, x: int, y: int):
-        super().__init__(x, y)
-
-    def __str__(self):
-        return "W"
+        super().__init__("W", x, y)
