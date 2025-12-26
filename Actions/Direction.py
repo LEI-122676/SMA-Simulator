d317012d9a941eaa1e5cbe619841b091d9dd34d5
Tiago Candeias <tfcss2@iscte-iul.pt>
2025-12-26 17:02:11 +0000

    Revert "diagrama de classes updated"

--- Actions/Direction.py
+++ Actions/Direction.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 
 class Direction(Enum):  # Add more directions for more complex movement (more rays for the raycasting sensors)
-    NORTH = (0, 1)
-    SOUTH = (0, -1)
-    WEST  = (1, 0)
-    EAST  = (-1, 0)
+    NORTH = (0, -1)
+    SOUTH = (0, 1)
+    WEST  = (-1, 0)
+    EAST  = (1, 0)
 
-    NORTH_EAST = (-1, 1)
-    NORTH_WEST = (1, 1)
-    SOUTH_EAST = (-1, -1)
-    SOUTH_WEST = (1, -1)
+    NORTH_EAST = (1, -1)
+    NORTH_WEST = (-1, -1)
+    SOUTH_EAST = (1, 1)
+    SOUTH_WEST = (-1, 1)
